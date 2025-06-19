794b9a2e151107c18274803ac623c61db218c76c
Thanattsitt S. <166811468+Pigletpeakkung@users.noreply.github.com>
2025-06-19 13:47:10 +0700

    Delete netlify/edge-functions/cosmic-greeting.js

--- netlify/edge-functions/cosmic-greeting.js
+++
@@ -1,7 +0,0 @@
-// netlify/edge-functions/cosmic-greeting.js
-export default async (request, context) => {
-  const country = context.geo?.country?.name || "unknown dimension";
-  return new Response(`Welcome, traveler from ${country}!`, {
-    headers: { "Content-Type": "text/html" },
-  });
-};
